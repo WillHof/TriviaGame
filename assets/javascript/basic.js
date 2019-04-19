fdb8f1f1e2d701d72d9db63313941cf349dd52a4
Will <whof@bu.edu>
2019-04-18 20:23:21 -0400

    js append is working, but problem with question answers

--- assets/javascript/basic.js
+++ assets/javascript/basic.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
         }
 
     }
+
     q1 = {
         qText: "What is the name of the instructor of the 2019 Harvard Coding Bootcamp?",
         choice1: ["Mark Techson", false],
@@ -23,31 +24,31 @@ $(document).ready(function () {
     }
     q2 = {
         qText: "Question",
-        choice1: ["something"],
-        choice2: ["something"],
-        choice3: ["something"],
-        choice4: ["something"]
+        choice1: ["something", true],
+        choice2: ["something", false],
+        choice3: ["something", false],
+        choice4: ["something", false]
     }
     q3 = {
         qText: "Question",
-        choice1: ["something"],
-        choice2: ["something"],
-        choice3: ["something"],
-        choice4: ["something"]
+        choice1: ["something", false],
+        choice2: ["something", false],
+        choice3: ["something", false],
+        choice4: ["something", true]
     }
     q4 = {
         qText: "Question",
-        choice1: ["something"],
-        choice2: ["something"],
-        choice3: ["something"],
-        choice4: ["something"]
+        choice1: ["something", false],
+        choice2: ["something", false],
+        choice3: ["something", false],
+        choice4: ["something", true]
     }
     q5 = {
         qText: "Question",
-        choice1: ["something"],
-        choice2: ["something"],
-        choice3: ["something"],
-        choice4: ["something"]
+        choice1: ["something", true],
+        choice2: ["something", false],
+        choice3: ["something", false],
+        choice4: ["something", false]
     }
     var qArray = [q1, q2, q3, q4, q5]
     console.log(qArray[0].choice1[0])
@@ -63,19 +64,24 @@ $(document).ready(function () {
             //creates variable with html string with class q[i]
             var qHTML = `<p class="h3" id="q` + i + `">` + currentQ.qText + `</p>`;
             var qID = `#q` + i + ``
+            var fID = `#f` + i + ``
+            var fHTML = `<form id="f` + i + `"></f>`
+            var k = 0
             console.log(qID)
             //adds the string to the start of the game div
             $("#appendQ").append(qHTML);
+            $(qID).append(fHTML)
             //goes through properties in current object, excluding native properties and qText
             for (var j in currentQ) {
                 if (currentQ.hasOwnProperty(j) && j !== "qText") {
                     var choiceHTML = `<div class="form-check form-check-inline">
-                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
-                         value="option1">
-                         <label class="form-check-label" for= "inlineRadio1" >` + currentQ[j][0] + `</label >
+                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio`+ k + `"
+                         value="option`+ k + `">
+                         <label class="form-check-label" for= "inlineRadio`+ k + `" >` + currentQ[j][0] + `</label >
                  </div > `
                 }
-                $(q0).append(choiceHTML)
+                $(fID).append(choiceHTML)
+                k++
                 //     var choice = "choice" + j;
                 //     //currentC should return the current question, currentQ.choice1, currentQ.choice2... need to convert the properties in the object into an array
                 //     var currentC = currentQ;
