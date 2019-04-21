f70e8791bd25c5490b9dca51bcb1d58b63e1c0fd
Will <whof@bu.edu>
2019-04-20 22:48:59 -0400

    updated

--- assets/javascript/basic.js
+++ assets/javascript/basic.js
@@ -1,20 +1,25 @@
 //this is not the simplest solution but i wanted to learn about objects
 
 $(document).ready(function () {
-    var time = 62
-    intervalID = setInterval(counter, 1000)
-    $("#timeRemaining").html(time)
+    var time = 60;
+    intervalID = setInterval(counter, 1000);
+    $("#timeRemaining").html(time);
+    function gameFinished() {
 
+    }
     function counter() {
-        time--
-        $("#timeRemaining").html(time)
+        time--;
+        $("#timeRemaining").html(time);
         if (time === 0) {
-            clearInterval(intervalID)
-
+            clearInterval(intervalID);
+            $("#gameEnd").toggleClass("off")
+            $("#gameStart").toggleClass("off")
+            gameFinished()
         }
 
     }
-
+    document.addEventListener('click', document.getElementsByClassName("form-check-input"), function () { console.log(this) })
+    $(document).on("click", $(".form-check-input"), function () { console.log(this) })
     q1 = {
         qText: "What is the name of the instructor of the 2019 Harvard Coding Bootcamp?",
         choice1: ["Mark Techson", false],
@@ -27,67 +32,70 @@ $(document).ready(function () {
         choice1: ["something", true],
         choice2: ["something", false],
         choice3: ["something", false],
-        choice4: ["something", false]
+        choice4: ["test", false]
     }
     q3 = {
         qText: "Question",
         choice1: ["something", false],
         choice2: ["something", false],
         choice3: ["something", false],
-        choice4: ["something", true]
+        choice4: ["this", true]
     }
     q4 = {
         qText: "Question",
         choice1: ["something", false],
         choice2: ["something", false],
         choice3: ["something", false],
-        choice4: ["something", true]
+        choice4: ["out", true]
     }
     q5 = {
         qText: "Question",
         choice1: ["something", true],
         choice2: ["something", false],
         choice3: ["something", false],
-        choice4: ["something", false]
+        choice4: ["now", false]
     }
     var qArray = [q1, q2, q3, q4, q5]
-    console.log(qArray[0].choice1[0])
 
-
-    $(".btn").click(function () {
-        $("#gameStart").toggleClass("off");
-        $(".btn").toggleClass("off");
-        counter()
+    function loadQuestion() {
         for (i = 0; i < qArray.length; i++) {
             //gets current object in the array
             var currentQ = qArray[i]
             //creates variable with html string with class q[i]
             var qHTML = `<p class="h3" id="q` + i + `">` + currentQ.qText + `</p>`;
-            var qID = `#q` + i + ``
-            var fID = `#f` + i + ``
-            var fHTML = `<form id="f` + i + `"></f>`
-            var k = 0
-            console.log(qID)
-            //adds the string to the start of the game div
+            //JQ selector for current question element
+            var qID = `#q` + i + ``;
+            //JQ selector for current form element
+            var fID = `#f` + i + ``;
+            //unique ids per form 
+            var fHTML = `<form id="f` + i + `"></f>`;
+            //IDS for the question answers
+            var k = 0;
+            //creates the question & form element
             $("#appendQ").append(qHTML);
             $(qID).append(fHTML)
-            //goes through properties in current object, excluding native properties and qText
+            //creates an html element for each property in the question
+            //want to have a check at the object level if the answer is correct. if it is, it will give the answer an attribute of correct. if(currentQ.j[1]===true)... however, this is always giving me undefined. 
             for (var j in currentQ) {
                 if (currentQ.hasOwnProperty(j) && j !== "qText") {
+
                     var choiceHTML = `<div class="form-check form-check-inline">
                      <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio`+ k + `"
                          value="option`+ k + `">
                          <label class="form-check-label" for= "inlineRadio`+ k + `" >` + currentQ[j][0] + `</label >
                  </div > `
+                    $(fID).append(choiceHTML)
+
                 }
-                $(fID).append(choiceHTML)
                 k++
-                //     var choice = "choice" + j;
-                //     //currentC should return the current question, currentQ.choice1, currentQ.choice2... need to convert the properties in the object into an array
-                //     var currentC = currentQ;
-
-
             }
         }
+    }
+    $(".btn").click(function () {
+        $("#gameStart").toggleClass("off");
+        $(".btn").toggleClass("off");
+        counter()
+        loadQuestion()
+
     });
-});
+})
